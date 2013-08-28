2975a996b27457074d6c3eb27a3fec28323fb11b
Tom Ashworth <tom@phuu.net>
2013-08-28 20:07:28 +0100

    Build the damn thing.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-var connect = require('connect');
-var urlp = require('./lib/urlp');
+var pkg      = require('./package.json');
+var connect  = require('connect');
+var urlp     = require('./lib/urlp');
+var pulldown = new (require('pulldown'))();
+
+String.supplant = function (str, o) {
+  return str.replace(/{([^{}]*)}/g,
+    function (a, b) {
+      var r = o[b];
+      return typeof r === 'string' || typeof r === 'number' ? r : a;
+    }
+  );
+};
+
+var error = function (msg) {
+  return 'console.log("pldn.io: ' + msg + '")';
+};
 
 connect()
   .use(urlp.mw.parse)
+  .use(function (req, res, next) {
+    pulldown.init(req.args, function (err, files) {
+      if (err) { return res.send(500, error('Pulldown module failed.')); }
+      req.files = files;
+      next();
+    });
+  })
   .use(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(JSON.stringify(req.args));
+    var results = req.files.map(function (file) {
+      return file.contents;
+    });
+    results.unshift(String.supplant('/* Served by pldn.io v{version} */', pkg));
+    res.end(results.join('\n'));
   })
   .listen(9123, function () {
     console.log('pldn.io @ http://localhost:' + this.address().port);
