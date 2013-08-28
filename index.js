16cd4c3e1a35bc3d529b4020705c63cf9c8b7c85
Tom Ashworth <tom@phuu.net>
2013-08-28 21:45:02 +0100

    Add in memory caching.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var connect  = require('connect');
 var urlp     = require('./lib/urlp');
 var pulldown = new (require('pulldown'))();
 var supplant = require('./lib/supplant');
+var cache    = require('./lib/cache');
 
 var errorStr = function (msg) {
   return '(\'console\' in window) && console.log("pldn.io: ' + msg + '")';
@@ -12,6 +13,10 @@ connect()
   .use(connect.logger('dev'))
   .use(connect.compress())
   .use(connect.favicon())
+  .use(function (req, _, next) {
+    req.pldn = {};
+    next();
+  })
   // Parse the URL
   .use(urlp.mw.parse)
   // Set the content type as javascript
@@ -19,26 +24,40 @@ connect()
     res.setHeader("Content-Type", "application/javascript");
     next();
   })
+  // Cache
+  .use(cache.mw.get)
   // Grab the files from pulldown
   .use(function (req, res, next) {
     pulldown.init(req.args, function (err, files) {
       if (err) { return next('Pulldown module failed.'); }
-      req.files = files;
+      req.pldn.files = files;
       next();
     });
   })
-  // Produce the final file
-  .use(function (req, res) {
-    var results = req.files.map(function (file) {
+  // Produce the final response
+  .use(function (req, res, next) {
+    var results = req.pldn.files.map(function (file) {
       return file.contents;
     });
     results.unshift(supplant('/** Served with <3 by pldn.io v{version} **/', pkg));
-    res.writeHead(200, 'Awesome', {});
-    res.end(results.join('\n'));
+    req.pldn.response = results.join('\n');
+    next();
+  })
+  // Cache the response
+  .use(function (req, res, next) {
+    cache.set(req.url, req.pldn.response, function (err) {
+      if (err) { return next(err); }
+      next();
+    });
+  })
+  // Serve the response
+  .use(function (req, res) {
+    res.statusCode = 200;
+    res.end(req.pldn.response);
   })
   // Error handler
   .use(function (err, req, res, next) {
-    console.log('err', err);
+    res.statusCode = 500;
     res.end(errorStr(err.message || err));
   })
   .listen(process.env.PORT || 0, function () {
